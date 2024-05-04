02dd311c99d693af22e996ea92499e9b9dea2a2b
ksy7615 <sykim7615@daum.net>
2024-05-05 00:33:52 +0900

    Add: DB Connection

---
+++ src/main/java/rentcarServer/DBConnection.java
@@ -0,0 +1,51 @@
+package rentcarServer;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class DBConnection {
+	public static Connection getConnection() {
+        Connection conn = null;
+        
+        try {
+            Context init = new InitialContext();
+            DataSource ds = (DataSource) init.lookup("java:comp/env/jdbc/RentcarServerDB");
+            
+            conn = ds.getConnection();
+            
+            System.out.println("DB연동 성공");
+            return conn;
+        } catch (Exception e) {
+            System.out.println("DB연동 실패");
+        }
+        return null;
+    }
+    
+    public static void close(Connection conn, PreparedStatement pstmt , ResultSet rs) {
+        try {
+            rs.close();
+            pstmt.close();
+            conn.close();
+            
+            System.out.println("DB연결 해제");
+        } catch (Exception e) {
+            System.out.println("DB연결 해제 실패");
+        }
+    }
+    
+    public static void close(Connection conn, PreparedStatement pstmt) {
+        try {
+            pstmt.close();
+            conn.close();
+            
+            System.out.println("DB연결 해제");
+        } catch (Exception e) {
+            System.out.println("DB연결 해제 실패");
+        }
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,3 +1,5 @@
+<%@page import="rentcarServer.DBConnection"%>
+<%@page import="java.sql.Connection"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html>
@@ -8,5 +10,8 @@
 </head>
 <body>
 	<h1>Hello World</h1>
+	<%
+		Connection conn = DBConnection.getConnection();
+	%>
 </body>
 </html>
