8e75b2b6b86e234316201d479fe5b66bfde6e0bb
ksy7615 <sykim7615@daum.net>
2024-05-13 03:58:40 +0900

    Add: 예약 가능한 차량만 확인 >> 값 넘겨주는 용도

---
+++ src/main/java/rentcarServer/car/controller/PagingAction.java
@@ -0,0 +1,30 @@
+package rentcarServer.car.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import rentcarServer.car.model.CarDao;
+import rentcarServer.car.model.CarResponseDto;
+
+public class PagingAction extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		CarDao carDao = CarDao.getInstance();
+		String startDate = request.getParameter("start-datetime");
+		String endDate = request.getParameter("end-datetime");
+		List<CarResponseDto> carList = carDao.findValidCars(startDate, endDate);
+		
+		request.setAttribute("start-datetime", startDate);
+		request.setAttribute("end-datetime", endDate);
+		request.setAttribute("carList", carList);
+		
+		request.setAttribute("carList", carList);
+		request.getRequestDispatcher("/").forward(request, response);
+	}
+}
