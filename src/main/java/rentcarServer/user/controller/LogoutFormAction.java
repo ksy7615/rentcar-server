a2a0335fcaceba128ef7b0b1273191e1cc445dbe
ksy7615 <sykim7615@daum.net>
2024-05-13 03:56:07 +0900

    Add: logout Action controller

--- src/main/java/rentcarServer/user/controller/LogoutFormAction.java
+++ src/main/java/rentcarServer/user/controller/LogoutFormAction.java
@@ -8,10 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class LogoutAction
- */
-@WebServlet("/LogoutFormAction")
 public class LogoutFormAction extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
