4e3bb81cb964193f9c77e5c2335b9dd9b434e865
ksy7615 <sykim7615@daum.net>
2024-05-13 03:58:05 +0900

    Modify: DeleteBoardFormAction

--- src/main/java/rentcarServer/board/controller/DeleteBoardFormAction.java
+++ src/main/java/rentcarServer/board/controller/DeleteBoardFormAction.java
@@ -17,58 +17,16 @@ import rentcarServer.board.model.BoardResponseDto;
 /**
  * Servlet implementation class DeleteFormAction
  */
-@WebServlet("/DeleteBoardFormAction")
 public class DeleteBoardFormAction extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public DeleteBoardFormAction() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		HttpSession session = request.getSession();
 
 		BoardDao boardDao = BoardDao.getInstance();
-		List<BoardResponseDto> list = boardDao.findBoardAll();
-		String id = (String) session.getAttribute("userId");
-		String boardCode = (String) session.getAttribute("boardCode");
+		String boardCode = request.getParameter("boardCode");
+		int boardId = Integer.parseInt(boardCode);
 		
-		boolean result = false;
-		for (BoardResponseDto board : list) {
-			if (board.getUserId().equals(id)) {
-				request.setAttribute("board", board);						
-				int boardId = board.getBoardCode();
-				BoardRequestDto boardDto = new BoardRequestDto();
-				result = boardDao.deleteBoard(boardDto);
-			}
-		}
-		
-		if (result) {
-			session.removeAttribute("board");
-			request.getRequestDispatcher("/viewBoard").forward(request, response);
-		} else {
-			response.sendRedirect("/board");
-		}
+		boardDao.deleteBoard(boardId);
+		response.sendRedirect("/board");
 	}
-
 }
